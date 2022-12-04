6f32acb0df10dbeff8ab67f8e02152410d3c8141
Anders Mörtsell <anders.mortsell@sogeti.se>
2022-12-04 10:12:25 +0100

    Commpleted day 3

---
+++ Day 3/task 2.ts
@@ -0,0 +1,74 @@
+import * as fs from 'fs';
+
+const priorities = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4, 
+  e: 5,
+  f: 6, 
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10,
+  k: 11,
+  l: 12,
+  m: 13,
+  n: 14,
+  o: 15,
+  p: 16,
+  q: 17,
+  r: 18,
+  s: 19,
+  t: 20,
+  u: 21,
+  v: 22,
+  w: 23,
+  x: 24,
+  y: 25,
+  z: 26,
+  A: 27,
+  B: 28,
+  C: 29,
+  D: 30, 
+  E: 31,
+  F: 32, 
+  G: 33,
+  H: 34,
+  I: 35,
+  J: 36,
+  K: 37,
+  L: 38,
+  M: 39,
+  N: 40,
+  O: 41,
+  P: 42,
+  Q: 43,
+  R: 44,
+  S: 45,
+  T: 46,
+  U: 47,
+  V: 48,
+  W: 49,
+  X: 50,
+  Y: 51,
+  Z: 52
+}
+
+const input: string = fs.readFileSync('./Day 3/input.txt', 'utf8').toString();
+let total: number = 0;
+
+const lines = input.split('\r\n');
+for (let i = 0; i < lines.length - 2; i += 3) {
+  let found: string = ""
+  const first = lines[i];
+  const middle = lines[i + 1];
+  const last = lines[i + 2];
+  
+  first.split('').map(c => {
+    const regex: RegExp = new RegExp(c);
+    regex.test(middle) && regex.test(last) && found.indexOf(c) === -1 && (total += priorities[c], found += c);
+  });
+}
+
+console.log(total);
